3ce0952e082f3051559a0633681b0fb14d7bd1c8
Femi Akinyemi <akinfemi46@gmail.com>
2020-09-03 13:32:22 +0100

    Updates

---
+++ precache-manifest.874ac84f2a68fbdc4c26ae2a6819d91d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53042066d63df96295ef2adc022bf188",
+    "url": "/react_learning/index.html"
+  },
+  {
+    "revision": "7c1be3d79efe996c931c",
+    "url": "/react_learning/static/css/main.5a1ce5ca.chunk.css"
+  },
+  {
+    "revision": "0aa8577780a1a4cd9c26",
+    "url": "/react_learning/static/js/2.d2e25065.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_learning/static/js/2.d2e25065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c1be3d79efe996c931c",
+    "url": "/react_learning/static/js/main.deb4a5e8.chunk.js"
+  },
+  {
+    "revision": "bc4585beb12132300e9f",
+    "url": "/react_learning/static/js/runtime-main.f6cedef9.js"
+  }
+]);
