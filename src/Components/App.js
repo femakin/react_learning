809806af4414fcaa7a3da5dcfcf35dd86ca65e56
Femi Akinyemi <akinfemi46@gmail.com>
2020-09-03 14:07:01 +0100

    Added unique key to the mapped items

--- src/Components/App.js
+++ src/Components/App.js
@@ -9,26 +9,32 @@ class App extends React.Component {
 
     const languageList = [
       {
+        id: 1,
         name: 'HTML & CSS',
         image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg'
       },
       {
+        id: 2,
         name: 'JavaScript',
         image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/es6.svg'
       },
       {
+        id: 3,
         name: 'React',
         image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/react.svg'
       },
       {
+        id: 4,
         name: 'Ruby',
         image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/ruby.svg'
       },
       {
+        id: 5,
         name: 'Ruby on Rails',
         image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/rails.svg'
       },
       {
+        id: 6,
         name: 'Python',
         image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/python.svg'
       }
@@ -45,6 +51,7 @@ class App extends React.Component {
             return (
               // Call the Language component and pass props to it
               <Language 
+                key={languageItem.id}
                 name={languageItem.name}
                 image={languageItem.image}
               />
